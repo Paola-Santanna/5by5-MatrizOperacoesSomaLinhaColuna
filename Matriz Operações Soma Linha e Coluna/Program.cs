1916275a928034853c1f26c12e725bcc2f5a3453
Paola-Santanna <paolinhasj@gmail.com>
2024-04-24 14:42:03 -0300

    #Operações Desenvolvimento das operações de soma com as linhas.

--- Matriz Operações Soma Linha e Coluna/Program.cs
+++ Matriz Operações Soma Linha e Coluna/Program.cs
@@ -1,14 +1,18 @@
 ﻿//Uma matriz quadrada, de tamanho variado; mostre os resultados das operações de soma; os resultados devem ser referentes à soma de cada linha, à soma de cada coluna e à soma de cada diagonal;
-int qnt_linhas = 3, qnt_colunas = 3;
+int qnt_linhas = 3, qnt_colunas = 3, soma_linha = 0, soma_coluna, soma_diagonal_principal, soma_diagonal_sevundaria;
+int cont_parar = 0;
 
 int[,] matriz = new int[qnt_linhas, qnt_colunas];
+int[] resultado_soma_linhas = new int[qnt_linhas];
+int[] resultado_soma_colunas = new int[qnt_colunas];
+int[] resultado_soma_diagonais = new int[2];
 
 //Geração da matriz
 for (int linha = 0; linha < qnt_linhas; linha++)
 {
     for (int coluna = 0; coluna < qnt_colunas; coluna++)
     {
-        matriz[linha, coluna] = new Random().Next(0,10);
+        matriz[linha, coluna] = new Random().Next(0, 10);
     }
 }
 
@@ -19,14 +23,42 @@ for (int linha = 0; linha < qnt_linhas; linha++)
     Console.WriteLine();
     for (int coluna = 0; coluna < qnt_colunas; coluna++)
     {
-        Console.Write(matriz[linha,coluna] + " ");
+        Console.Write(matriz[linha, coluna] + " ");
     }
 }
 
 //Geração das operações com as linhas
+for (int linha = 0; linha < qnt_linhas; linha++)
+{
+
+    for (int coluna = 0; coluna < qnt_colunas; coluna++)
+    {
+        soma_linha += matriz[linha, coluna];
+    }
+    resultado_soma_linhas[linha] = soma_linha;
+    soma_linha = 0;
+}
+
+//Impressão das operações com as linhas
+Console.WriteLine("\n\nSoma das linhas:");
+for (int indice = 0; indice < qnt_linhas; indice++)
+{
+    Console.WriteLine($"{indice + 1}ª linha = {resultado_soma_linhas[indice]}");
+}
+
+//Geração das operações com as colunas
+for (int linha = 0; linha < qnt_linhas; linha++)
+{
+
+    for (int coluna = 0; coluna < qnt_colunas; coluna++)
+    {
+        soma_linha += matriz[linha, coluna];
+    }
+    resultado_soma_linhas[linha] = soma_linha;
+    soma_linha = 0;
+}
+
 //Impressão das operações com as colunas
-//Geração das operações com as diagoais
-//Impressão das operações com as diagonais
 
 Console.WriteLine("\n\nPressione ENTER para encerrar...");
 Console.ReadKey();
